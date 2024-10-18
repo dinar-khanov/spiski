668bbc35c1ae8c3a3135f701fbe5d308beeb151e
dinar-khanov <hanov.di@gmail.com>
2024-10-18 13:53:19 +0500

    Add files via upload

---
+++ Кортежи и списки.py
@@ -0,0 +1,6 @@
+immutable_var=1,2,3, True,"string" #Создайте переменную immutable_var и присвойте ей кортеж из нескольких элементов разных типов данных.
+print(immutable_var) #Выполните операции вывода кортежа immutable_var на экран
+#print(immutable_var)[0]=100 #Попытайтесь изменить элементы кортежа immutable_var.
+mutable_list=[1,2,3,4,5] #Создайте переменную mutable_list и присвойте ей список из нескольких элементов.
+mutable_list[0]=100 #Измените элементы списка mutable_list.
+print(mutable_list) #Выведите на экран измененный список mutable_list.
